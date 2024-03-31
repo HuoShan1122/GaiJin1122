d3de8d283b764183b1faab7c5c987cde8373d3bf
HuoShan1122 <xurongcan0218@163.com>
2024-03-31 20:14:01 +0800

    message 8: Before refactoring the functional_tests

--- functional_tests.py
+++ functional_tests.py
@@ -13,8 +13,8 @@ class NewVisitorTest(unittest.TestCase):
 
     def test_can_start_a_list_and_retrieve_it_later(self):
 
-        # 张三听说有一个在线代办事项的应用
-        # 它去看了这个应用的首页
+        # 张三听说有一个在线待办事项的应用
+        # 他去看了这个应用的首页
         self.browser.get('http://localhost:8000')
 
         # 他注意到网页里包含“To-Do"这个词
@@ -22,7 +22,7 @@ class NewVisitorTest(unittest.TestCase):
         header_text = self.browser.find_element(By.TAG_NAME, 'h1').text # (1)
         self.assertIn('To-Do', header_text)
 
-        # 应用有一个输入代办事项的文本输入框
+        # 应用有一个输入待办事项的文本输入框
         inputbox = self.browser.find_element(By.ID, 'id_new_item') # (1)
         self.assertEqual(inputbox.get_attribute('placeholder'),
                          'Enter a to-do item'
@@ -41,10 +41,23 @@ class NewVisitorTest(unittest.TestCase):
         self.assertIn('1: Buy flowers', [row.text for row in rows])
 
         # 页面中又显示了一个文本输入框，可以输入其他代办事项
-        # 它输入了”gift to girlfriend“
-        self.fail('Finish the test!')
+        # 它输入了”Give a gift to Lisi“
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        inputbox.send_keys('Give a gift to Lisi')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
         # 页面再次更新，他的清单中显示了这两个待办事项
+        table = self.browser.find_element(By.ID, 'id_list_table')
+        rows = table.find_elements(By.TAG_NAME, 'tr')
+        self.assertIn('1: Buy flowers', [row.text for row in rows])
+        self.assertIn('2: Give a gift to Lisi', [row.text for row in rows])
+
+        # 张三想知道这个网站是否会记住他的清单
+        # 他看到网站为他生成了一个唯一的URL
+
+        self.fail('Finish the test!')
+
 
 if __name__ == '__main__':
     unittest.main()
