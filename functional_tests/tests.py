c52833a5a001880e61a1b2ffbb340f45cef5db94
HuoShan1122 <xurongcan0218@163.com>
2024-05-22 20:45:21 +0800

    message 14: add a new user

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -54,7 +54,7 @@ class NewVisitorTest(LiveServerTestCase):
         self.wait_for_row_in_list_table('1: Buy flowers')
 
         # 页面中又显示了一个文本输入框，可以输入其他代办事项
-        # 它输入了”Give a gift to Lisi“
+        # 他输入了”Give a gift to Lisi“
         inputbox = self.browser.find_element(By.ID, 'id_new_item')
         inputbox.send_keys('Give a gift to Lisi')
         inputbox.send_keys(Keys.ENTER)
@@ -68,7 +68,49 @@ class NewVisitorTest(LiveServerTestCase):
 
         self.fail('Finish the test!')
 
+    def test_multiple_users_can_start_lists_at_different_urls(self):
+        # 张三新建一个待办事项清单
+        self.browser.get(self.live_server_url)
+        inputbox = self.browser.find_element(By.ID,'id_new_item')
+        inputbox.send_keys('Buy flowers')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy flowers')
+        # 他注意到清单有个唯一的 URL
+        zhangsan_list_url = self.browser.current_url
+        self.assertRegex(zhangsan_list_url,'/lists/.+') #(1)
 
-#if __name__ == '__main__':
-#   unittest.main()
+        # 现在一个新用户王五访问网站
+        # 我们使用一个新浏览器会话
+        # 确保张三的信息不会从cookie中泄露出去
+        self.browser.quit()
+        self.browser = webdriver.chrome()
+
+        # 王五访问首页
+        # 页面中看不到张三的清单
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element(By.TAG_NAME, 'body').text
+        self.assertNotIn('Buy flowers', page_text)
+        self.assertNotIn('Give a gift to Lisi', page_text)
+
+        # 王五输入一个新待办事项，新建一个清单
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy milk')
+
+        # 王五获得了他的唯一 URL
+        wangwu_list_url = self.browser.current_url
+        self.assertRegex(wangwu_list_url, '/lists/.+')
+        self.assertNotEqual(wangwu_list_url, zhangsan_list_url)
+
+        # 这个页面还是没有张三的清单
+        page_text = self.browser.find_element(By.TAG_NAME, 'body').text
+        self.assertNotIn('Buy flowers', page_text)
+        self.assertIn('Buy milk', page_text)
+
+        # 两人都很满意，然后去睡觉了
+
+
+
+
 
